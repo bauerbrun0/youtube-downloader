68aa69f4f7dcd380f61ede64405a5c6fecaeb633
bauerbrun0 <bauerbruno@protonmail.com>
2023-12-04 10:23:58 +0100

    Added GitHub workflow; Providing env variables to solid build at runtime is now possible using vite-plugin-runtime-env; Moved apps/elysia-api/Dockerfile to root, and reduced the image size

--- apps/solid-app/vite.config.ts
+++ apps/solid-app/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import runtimeEnv from "vite-plugin-runtime-env";
 
 export default defineConfig({
-	plugins: [solid()],
+	plugins: [
+		solid(),
+		runtimeEnv()
+	],
 	build: {
 		outDir: './dist',
 		emptyOutDir: true,
 	}
-})
+});
